64c81eb93dc787c7ed181475fb39999cad9ce6b3
psyomn <lethaljellybean@gmail.com>
2019-04-19 16:40:56 -0400

    Fix build issues

--- src/headers.ads
+++ src/headers.ads
@@ -11,6 +11,7 @@
 --  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 --  See the License for the specific language governing permissions and
 --  limitations under the License.with Interfaces; use Interfaces;
+with Interfaces; use Interfaces;
 package Headers is
 
    type Byte is range 0 .. 16#ff#;
